1d1b8e60811de309f20d42615f130c01a47184e8
unknown <english@GCC-PC6008.student.gcc.ie>
2016-03-30 11:35:21 +0100

    Milestone 1:

---
+++ app/src/main/java/com/example/english/assignment2/CustomView.java
@@ -0,0 +1,38 @@
+package com.example.english.assignment2;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by english on 30/03/2016.
+ */
+public class CustomView extends View {
+    public CustomView(Context c) {
+        super(c);
+        init();
+    }
+
+    public CustomView(Context c, AttributeSet as) {
+        super(c, as);
+        init();
+    }
+
+    public CustomView(Context c, AttributeSet as, int default_style) {
+        super(c, as, default_style);
+        init();
+    }
+
+    private void init() {
+    }
+
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+}
